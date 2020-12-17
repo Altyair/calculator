e838184336dcfe3d4e909816e54e52dd9af555d9
Altyair <altyair@mail.ru>
2020-12-17 10:33:47 +0300

    first

--- src/MathCalculator/Abstract/AbstractCalculatorCore.ts
+++ src/MathCalculator/Abstract/AbstractCalculatorCore.ts
@@ -108,7 +108,7 @@ export default abstract class AbstractCalculatorCore {
                 }
             } else {
 
-                if (lastItem.hasOwnProperty('value') && !lastItem.value) {
+                if (lastItem.hasOwnProperty('value') && lastItem.value === '') {
                     return true;
                 }
 
